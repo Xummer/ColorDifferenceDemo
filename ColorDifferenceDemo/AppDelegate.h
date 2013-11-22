b99763169d0d817ca3ed829bfc071eede19e4c64
Xummer <xummer26@gmail.com>
2013-11-22 10:19:33 +0800

    Initial Commit

---
+++ ColorDifferenceDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorDifferenceDemo
+//
+//  Created by Xummer on 13-11-22.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorDifferenceDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorDifferenceDemo
+//
+//  Created by Xummer on 13-11-22.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
